1ae384df8ac3b6ffc5dc6053f1cf46484fbf6bdd
Adam <adam.and.math@gmail.com>
2018-05-25 22:10:59 +0200

    Adding get test for generator and fixing bugs

--- src/generator.rs
+++ src/generator.rs
@@ -22,8 +22,10 @@ impl<E> FromIterator<(usize, E)> for Generator<E> {
         let mut total = 0;
 
         for (rand, item) in iter {
-            total += rand;
-            tree.insert(total, item);
+            if rand != 0 {
+                total += rand;
+                tree.insert(total - 1, item);
+            }
         }
 
         Generator { tree: tree, total: total }
@@ -56,4 +58,18 @@ impl<E> Generator<E> {
     pub fn iter<'a>(&'a self) -> Iter<'a, E> {
         Iter { generator: self, rng: rand::thread_rng() }
     }
+}
+
+#[test]
+fn manual_get() {
+    let g: Generator<&str> = vec![(10, "1st"), (0, "Nope"), (53, "2nd"), (1, "3rd")].into_iter().collect();
+
+    assert_eq!(g.get(0), Some(&"1st"));
+    assert_eq!(g.get(6), Some(&"1st"));
+    assert_eq!(g.get(9), Some(&"1st"));
+    assert_eq!(g.get(10), Some(&"2nd"));
+    assert_eq!(g.get(35), Some(&"2nd"));
+    assert_eq!(g.get(62), Some(&"2nd"));
+    assert_eq!(g.get(63), Some(&"3rd"));
+    assert_eq!(g.get(64), None);
 }

6fa7739f7672319b071beff0a5f59b38cee3ccb3
Adam <adam.and.math@gmail.com>
2018-12-26 02:52:51 +0100

    Fixed typo

--- src/generator.rs
+++ src/generator.rs
@@ -6,7 +6,7 @@ use rand;
 use rand::ThreadRng;
 use rand::Rng;
 
-/// Reterns random elements from a collection based on the weight of the element.
+/// Returns random elements from a collection based on the weight of the element.
 #[derive(Debug, Default)]
 pub struct Generator<E> {
     tree: BTreeMap<usize, E>,
@@ -116,25 +116,4 @@ impl<E> Generator<E> {
     pub fn generate<'a>(&'a self) -> Iter<'a, E> {
         Iter { generator: self, rng: rand::thread_rng() }
     }
-}
-
-#[test]
-fn manual_get() {
-    let g: Generator<&str> = vec![(10, "1st"), (0, "Nope"), (53, "2nd"), (1, "3rd")].into_iter().collect();
-
-    assert_eq!(g.get(0), Some(&"1st"));
-    assert_eq!(g.get(6), Some(&"1st"));
-    assert_eq!(g.get(9), Some(&"1st"));
-    assert_eq!(g.get(10), Some(&"2nd"));
-    assert_eq!(g.get(35), Some(&"2nd"));
-    assert_eq!(g.get(62), Some(&"2nd"));
-    assert_eq!(g.get(63), Some(&"3rd"));
-    assert_eq!(g.get(64), None);
-}
-
-#[test]
-fn empty_generator() {
-    let g: Generator<&str> = vec![].into_iter().collect();
-    assert_eq!(g.get(0), None);
-    assert_eq!(g.next(), None);
 }

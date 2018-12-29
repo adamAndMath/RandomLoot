ab4b6908feafb8bb86531c88d61880eccc7da4f4
Adam <adam.and.math@gmail.com>
2018-12-29 17:10:03 +0100

    Asking for amounts on runtime

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ pub mod quantifier;
 
 use group::Group;
 use std::env;
+use std::io::{ stdin, stdout, Write };
 
 fn main() {
     if let Err(e) = process() {
@@ -26,13 +27,30 @@ fn process() -> Result<(), String> {
 
     let path = args.next().ok_or("Requires path")?;
 
-    let amount = args
-        .next()
-        .map_or(Ok(1), |p| p.parse())
-        .map_err(|e| format!("Failed to parse amount, do to {}", e))?;
+    if let Some(arg) = args.next() {
+        let amount = arg.parse().map_err(|e| format!("Failed to parse amount, do to {}", e))?;
 
-    let group = Group::from_path(path).map_err(|e| format!("{}", e))?;
+        let group = Group::from_path(path).map_err(|e| format!("{}", e))?;
 
-    group.print(amount);
+        group.print(amount);
+    } else {
+        let group = Group::from_path(path).map_err(|e| format!("{}", e))?;
+
+        let mut input = String::new();
+        loop {
+            print!("amount: ");
+            stdout().flush().unwrap();
+            stdin().read_line(&mut input).unwrap();
+            {
+                let input = input.trim();
+                if input.is_empty() {break;}
+                match input.parse::<usize>() {
+                    Ok(amount) => group.print(amount),
+                    Err(e) => println!("Failed to parse amount, do to {}", e),
+                }
+            }
+            input.clear();
+        }
+    }
     Ok(())
 }

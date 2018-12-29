67d3b0a4f57956b602a9813e5ea77cdc31e8c30c
Adam <adam.and.math@gmail.com>
2018-12-29 16:56:58 +0100

    Benching line reading

--- src/tests/format/mod.rs
+++ src/tests/format/mod.rs
@@ -3,6 +3,12 @@ mod unit;
 mod var;
 
 #[bench]
-fn bench_on_of_each(b: &mut test::Bencher) {
+fn bench_one_of_each(b: &mut test::Bencher) {
     b.iter(|| "a: bool; b: int; c: float; d: str".parse::<::format::Format>());
+}
+
+#[bench]
+fn bench_one_line(b: &mut test::Bencher) {
+    let format = "a: bool; b: int; c: float; d: str".parse::<::format::Format>().unwrap();
+    b.iter(|| format.parse("true; 742; 5.23; House; 10"));
 }
